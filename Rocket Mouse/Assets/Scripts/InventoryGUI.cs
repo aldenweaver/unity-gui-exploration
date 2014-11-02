568b66a8dc72c1f1256899a61d48b1d9223d8bbd
aldenweaver <aaldenweaver@gmail.com>
2014-11-02 01:39:59 -0700

    Completed Part 1/3 of the tutorial
    Start and settings buttons; start is functional.

---
+++ Rocket Mouse/Assets/Scripts/InventoryGUI.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InventoryGUI : MonoBehaviour {
+
+	private Rect inventoryWindowRect = new Rect(300, 100, 400, 400);
+	private bool inventoryWindowShow = false;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
